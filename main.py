c3f2a06f0d76d3be50caf8de3761763dda8be7c4
Aditya <adi1097.ac@gmail.com>
2021-05-18 21:34:16 +0530

    cleaned code added info

--- main.py
+++ main.py
@@ -108,36 +108,36 @@ if page == 'Screener':
     with st.beta_expander('More Info'):
         st.write('''
         Return on Capital \n
-EBIT/(Net Working Capital + Net Fixed Assets)\n
-Return on capital was measured by calculating the ratio of pretax operating earnings (EBIT) to tangible capital employed (Net Working Capital + Net Fixed Assets). This ratio was used rather than the more commonly used ratios of return on equity (ROE, earnings/equity) or
-return on assets (ROA, earnings/assets) for several reasons. EBIT (or earnings before interest and taxes) was used in place of reported earnings because companies operate with different levels of debt and differing tax rates. Using operating earnings before interest and taxes, or EBIT, allowed us to view and compare the operating
-earnings of different companies without the distortions arising from differences in tax rates and debt levels. For each company, it was then
-possible to compare actual earnings from operations (EBIT) to the cost of the assets used to produce those earnings (tangible capital
-employed).\n
-Net Working Capital + Net Fixed Assets (or tangible capital employed) was used in place of total assets (used in an ROA calculation) or equity (used in an ROE calculation). The idea here was to figure out how much capital is actually needed to conduct the company’s business. Net working capital was used because a company has to fund its receivables and inventory (excess cash not needed to conduct the business was excluded from this calculation) but does not have to lay out money for its payables, as these are effectively an interest-free loan (short-term
-interest-bearing debt was excluded from current liabilities for this calculation). In addition to working capital requirements, a company must also
-fund the purchase of fixed assets necessary to conduct its business, such as real estate, plant, and equipment. The depreciated net cost of
-these fixed assets was then added to the net working capital requirements already calculated to arrive at an estimate for tangible capital employed.
-\nNOTE: Intangible assets, specifically goodwill, were excluded from the tangible capital employed calculations. Goodwill usually arises as a
-result of an acquisition of another company. The cost of an acquisition in excess of the tangible assets acquired is usually assigned to a
-goodwill account. In order to conduct its future business, the acquiring company usually only has to replace tangible assets, such as plant and equipment. Goodwill is a historical cost that does not have to be constantly replaced. Therefore, in most cases, return on tangible
-capital alone (excluding goodwill) will be a more accurate reflection of a business’s return on capital going forward. The ROE and ROA
-calculations used by many investment analysts are therefore often distorted by ignoring the difference between reported equity and assets and tangible equity and assets in addition to distortions due to differing tax rates and debt levels.
+        EBIT/(Net Working Capital + Net Fixed Assets)\n
+        Return on capital was measured by calculating the ratio of pretax operating earnings (EBIT) to tangible capital employed (Net Working Capital + Net Fixed Assets). This ratio was used rather than the more commonly used ratios of return on equity (ROE, earnings/equity) or
+        return on assets (ROA, earnings/assets) for several reasons. EBIT (or earnings before interest and taxes) was used in place of reported earnings because companies operate with different levels of debt and differing tax rates. Using operating earnings before interest and taxes, or EBIT, allowed us to view and compare the operating
+        earnings of different companies without the distortions arising from differences in tax rates and debt levels. For each company, it was then
+        possible to compare actual earnings from operations (EBIT) to the cost of the assets used to produce those earnings (tangible capital
+        employed).\n
+        Net Working Capital + Net Fixed Assets (or tangible capital employed) was used in place of total assets (used in an ROA calculation) or equity (used in an ROE calculation). The idea here was to figure out how much capital is actually needed to conduct the company’s business. Net working capital was used because a company has to fund its receivables and inventory (excess cash not needed to conduct the business was excluded from this calculation) but does not have to lay out money for its payables, as these are effectively an interest-free loan (short-term
+        interest-bearing debt was excluded from current liabilities for this calculation). In addition to working capital requirements, a company must also
+        fund the purchase of fixed assets necessary to conduct its business, such as real estate, plant, and equipment. The depreciated net cost of
+        these fixed assets was then added to the net working capital requirements already calculated to arrive at an estimate for tangible capital employed.
+        \nNOTE: Intangible assets, specifically goodwill, were excluded from the tangible capital employed calculations. Goodwill usually arises as a
+        result of an acquisition of another company. The cost of an acquisition in excess of the tangible assets acquired is usually assigned to a
+        goodwill account. In order to conduct its future business, the acquiring company usually only has to replace tangible assets, such as plant and equipment. Goodwill is a historical cost that does not have to be constantly replaced. Therefore, in most cases, return on tangible
+        capital alone (excluding goodwill) will be a more accurate reflection of a business’s return on capital going forward. The ROE and ROA
+        calculations used by many investment analysts are therefore often distorted by ignoring the difference between reported equity and assets and tangible equity and assets in addition to distortions due to differing tax rates and debt levels.
 
- Earnings Yield\n
-EBIT/ Enterprise Value\n
-Earnings yield was measured by calculating the ratio of pretax operating earnings (EBIT) to enterprise value (market value of equity + net
-interest-bearing debt). This ratio was used rather than the more commonly used P/E ratio (price/earnings ratio) or E/P ratio (earnings/price
-ratio) for several reasons. The basic idea behind the concept of earnings yield is simply to figure out how much a business earns relative to the
-purchase price of the business.\n
-Enterprise value was used instead of merely the price of equity (i.e., total market capitalization, share price multiplied by shares outstanding) because enterprise value takes into account both the price paid for an equity stake in a business as well as the debt financing
-used by a company to help generate operating earnings. By using EBIT (which looks at actual operating earnings before interest expense and
-taxes) and comparing it to enterprise value, we can calculate the pretax earnings yield on the full purchase price of a business (i.e., pretax operating earnings relative to the price of equity plus any debt assumed). This allows us to put companies with different levels of debt and
-different tax rates on an equal footing when comparing earnings yields. \n
-For example, in the case of an office building purchased for $1 million with an $800,000 mortgage and $200,000 in equity, the price of equity is $200,000, but the enterprise value is $1 million. If the building generates EBIT (earnings before interest and taxes) of $100,000, then EBIT/EV or the pretax earnings yield would be 10 percent ($100,000/ $1,000,000). However, the use of debt can greatly skew apparent returns
-from the purchase of these same assets when only the price of equity is considered. Assuming an interest rate of 6 percent on an $800,000 mortgage and a 40 percent corporate tax rate, the pretax earnings yield on our equity purchase price of $200,000 would appear to be 26
-percent.42 As debt levels change, this pretax earnings yield on equity would keep changing, yet the $1 million cost of the building and the
-$100,000 EBIT generated by that building would remain unchanged. In other words, P/E and E/P are greatly influenced by changes in debt
-levels and tax rates, while EBIT/EV is not.
+        Earnings Yield\n
+        EBIT/ Enterprise Value\n
+        Earnings yield was measured by calculating the ratio of pretax operating earnings (EBIT) to enterprise value (market value of equity + net
+        interest-bearing debt). This ratio was used rather than the more commonly used P/E ratio (price/earnings ratio) or E/P ratio (earnings/price
+        ratio) for several reasons. The basic idea behind the concept of earnings yield is simply to figure out how much a business earns relative to the
+        purchase price of the business.\n
+        Enterprise value was used instead of merely the price of equity (i.e., total market capitalization, share price multiplied by shares outstanding) because enterprise value takes into account both the price paid for an equity stake in a business as well as the debt financing
+        used by a company to help generate operating earnings. By using EBIT (which looks at actual operating earnings before interest expense and
+        taxes) and comparing it to enterprise value, we can calculate the pretax earnings yield on the full purchase price of a business (i.e., pretax operating earnings relative to the price of equity plus any debt assumed). This allows us to put companies with different levels of debt and
+        different tax rates on an equal footing when comparing earnings yields. \n
+        For example, in the case of an office building purchased for $1 million with an $800,000 mortgage and $200,000 in equity, the price of equity is $200,000, but the enterprise value is $1 million. If the building generates EBIT (earnings before interest and taxes) of $100,000, then EBIT/EV or the pretax earnings yield would be 10 percent ($100,000/ $1,000,000). However, the use of debt can greatly skew apparent returns
+        from the purchase of these same assets when only the price of equity is considered. Assuming an interest rate of 6 percent on an $800,000 mortgage and a 40 percent corporate tax rate, the pretax earnings yield on our equity purchase price of $200,000 would appear to be 26
+        percent.42 As debt levels change, this pretax earnings yield on equity would keep changing, yet the $1 million cost of the building and the
+        $100,000 EBIT generated by that building would remain unchanged. In other words, P/E and E/P are greatly influenced by changes in debt
+        levels and tax rates, while EBIT/EV is not.
 
         ''')
